1ad0fd439d3251cf13043cfe1a34471adf7c466e
gmonteiro13 <gscmonteiro@gmail.com>
2020-12-08 14:54:31 -0300

    Visitor open home page successfully

---
+++ spec/features/visitor_open_home_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'Visitor open home page' do
+    scenario 'successfully' do
+        visit root_path
+
+        expect(page).to have_content('Brick Busters')
+        expect(page).to have_content('Seja bem-vindo à sua plataforma de aluguel e revisão de filmes!')
+    end
+end
